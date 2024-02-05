dc8bf7d37d1033e60e0d24acab6db43c3d20e501
Mandeep <mandeep95018986@gmail.com>
2024-02-06 00:06:52 +0530

    add coolAnimation

--- script.js
+++ script.js
@@ -7,18 +7,41 @@ console.log(valuebtn.innerHTML);
 let value = 0;
 const valueChanger = (value) => {
     valuebtn.innerHTML = `<h1>${value}</h1>`;
+    
 }
 
-const coolAnimation = (num) => {
-    while(num>=0){
-        console.log(num);
-        num--;
+const coolAnimation = () => {
+    let i = value;
+
+    loopIteration = () => {
+        valueChanger(i);
+        value =0;
+        if(i<=0){
+            const pos = setTimeout(loopIteration, 27);
+            ++i;
+            if(i >=0){
+                valueChanger(0);
+                clearTimeout(pos);
+            }
+        }
+        else{
+            const neg = setTimeout(loopIteration, 27);
+            --i;
+            if(i<=0){
+                valueChanger(0);
+                clearTimeout(neg);
+            }
+            // console.log("STill called", i);
+        }
     }
+    loopIteration();
 };
 
-coolAnimation(20);
+// coolAnimation(20);
 
 
 decrease.addEventListener('click', () => valueChanger(--value))
 increase.addEventListener('click', ()=>valueChanger(++value))
-reset.addEventListener('click', () => (value = 0,valueChanger(0)))
+// reset.addEventListener('click', () => (value = 0,valueChanger(0)))
+
+reset.addEventListener('click', coolAnimation)
